f371ba993308149945ad5ff290a678e0be268c0a
John Foley <jfoley@cs.umass.edu>
2017-01-31 11:23:35 -0500

    Start working on "starter files" for p1 which will actually be a different sort of web application

---
+++ src/main/java/edu/mtholyoke/cs341bd/writr/Main.java
@@ -0,0 +1,7 @@
+package edu.mtholyoke.cs341bd.writr;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("writr is a cool name for a startup, right?");
+  }
+}
