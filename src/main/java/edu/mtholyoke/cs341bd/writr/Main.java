7a709206204b410b4555e828d281a1426c750a5f
John Foley <jfoley@cs.umass.edu>
2017-01-31 16:00:00 -0500

    No function change, just commenting things.

--- src/main/java/edu/mtholyoke/cs341bd/writr/Main.java
+++ src/main/java/edu/mtholyoke/cs341bd/writr/Main.java
@@ -3,11 +3,24 @@ package edu.mtholyoke.cs341bd.writr;
 import java.io.IOException;
 
 public class Main {
+  /**
+   * This is the entrance point of the Writr server. It parses arguments and starts up the application.
+   * @param args command line arguments -- you shouldn't have to fiddle with these; mostly so that I can run multiple copies (if I need to) of final results.
+   * @throws IOException the server looks for files on disk, so this could crash!
+   */
   public static void main(String[] args) throws IOException {
-    // use port 1234 or the first argument specified to this program.
-    int port = args.length > 0 ? Integer.parseInt(args[0]) : 1234;
-    // hosting configuration: is this at the root of a webserve? default -- yes.
-    String baseURL = args.length > 1 ? args[1] : "/";
+    int port = 1234;
+    String baseURL = "/";
+
+
+    if(args.length > 0) {
+      // use port 1234 or the first argument specified to this program.
+      port = Integer.parseInt(args[0]);
+    }
+    if(args.length > 1) {
+      // hosting configuration: is this at the root of a webserver? default -- yes.
+      baseURL = args[1];
+    }
 
     WritrServer app = new WritrServer(baseURL, port);
     try {
--- src/main/java/edu/mtholyoke/cs341bd/writr/WritrServer.java
+++ src/main/java/edu/mtholyoke/cs341bd/writr/WritrServer.java
@@ -30,26 +30,33 @@ public class WritrServer extends AbstractHandler {
     this.metaURL = "<base href=\""+baseURL+"\">";
     jettyServer = new Server(port);
 
+    // We create a ContextHandler, since it will catch requests for us under a specific path.
+    // This is so that we can delegate to Jetty's default ResourceHandler to serve static files, e.g. CSS & images.
     ContextHandler staticCtx = new ContextHandler();
     staticCtx.setContextPath("/static");
     ResourceHandler resources = new ResourceHandler();
     resources.setBaseResource(Resource.newResource("static/"));
     staticCtx.setHandler(resources);
 
+    // This context handler just points to the "handle" method of this class.
     ContextHandler defaultCtx = new ContextHandler();
     defaultCtx.setContextPath("/");
     defaultCtx.setHandler(this);
 
+    // Tell Jetty to use these handlers in the following order:
     ContextHandlerCollection collection = new ContextHandlerCollection();
     collection.addHandler(staticCtx);
     collection.addHandler(defaultCtx);
-
     jettyServer.setHandler(collection);
   }
 
+  /**
+   * Once everything is set up in the constructor, actually start the server here:
+   * @throws Exception if something goes wrong.
+   */
   public void run() throws Exception {
     jettyServer.start();
-    jettyServer.join(); // wait for it to finish here!
+    jettyServer.join(); // wait for it to finish here! We're using threads behind the scenes; so this keeps the main thread around until something can happen!
   }
 
   public String getStaticURL(String resource) {
@@ -70,6 +77,11 @@ public class WritrServer extends AbstractHandler {
     output.println("</div>");
   }
 
+  /**
+   * HTML top boilerplate; put in a function so that I can use it for all the pages I come up with.
+   * @param html where to write to; get this from the HTTP response.
+   * @param title the title of the page, since that goes in the header.
+   */
   private void printWritrPageStart(PrintWriter html, String title) {
     html.println("<html>");
     html.println("  <head>");
@@ -80,11 +92,24 @@ public class WritrServer extends AbstractHandler {
     html.println("  <body>");
     html.println("  <h1 class=\"logo\">Writr</h1>");
   }
+  /**
+   * HTML bottom boilerplate; close all the tags we open in printWritrPageStart.
+   * @param html where to write to; get this from the HTTP response.
+   */
   private void printWritrPageEnd(PrintWriter html) {
     html.println("  </body>");
     html.println("</html>");
   }
 
+  /**
+   * The main callback from Jetty.
+   * @param resource what is the user asking for from the server?
+   * @param jettyReq the same object as the next argument, req, just cast to a jetty-specific class (we don't need it).
+   * @param req http request object -- has information from the user.
+   * @param resp http response object -- where we respond to the user.
+   * @throws IOException -- If the user hangs up on us while we're writing back or gave us a half-request.
+   * @throws ServletException -- If we ask for something that's not there, this might happen.
+   */
   @Override
   public void handle(String resource, Request jettyReq, HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
     System.out.println(jettyReq);
@@ -125,43 +150,48 @@ public class WritrServer extends AbstractHandler {
     }
   }
 
+  /**
+   * When a user submits (enter key) or pressed the "Write!" button, we'll get their request in here. This is called explicitly from handle, above.
+   * @param req -- we'll grab the form parameters from here.
+   * @param resp -- where to write their "success" page.
+   * @throws IOException again, real life happens.
+   */
   private void handleForm(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     Map<String, String[]> parameterMap = req.getParameterMap();
 
     // if for some reason, we have multiple "message" fields in our form, just put a space between them, see Util.join.
+    // Note that message comes from the name="message" parameter in our <input> elements on our form.
     String text = Util.join(parameterMap.get("message"));
 
     if(text != null) {
       // Good, got new message from form.
       resp.setStatus(HttpServletResponse.SC_ACCEPTED);
       messageList.add(new WritrMessage(text));
-      setupRedirectPage(resp);
+
+      // Respond!
+      try (PrintWriter html = resp.getWriter()) {
+        printWritrPageStart(html, "Writr: Submitted!");
+        // Print actual redirect directive:
+        html.println("<meta http-equiv=\"refresh\" content=\"3; url=front \">");
+
+        // Thank you, link.
+        html.println("<div class=\"body\">");
+        html.println("<div class=\"thanks\">");
+        html.println("<p>Thanks for your Submission!</p>");
+        html.println("<a href=\"front\">Back to the front page...</a> (automatically redirect in 3 seconds).");
+        html.println("</div>");
+        html.println("</div>");
+
+        printWritrPageEnd(html);
+
+      } catch (IOException ignored) {
+        // Don't consider a browser that stops listening to us after submitting a form to be an error.
+      }
+
       return;
     }
 
     // user submitted something weird.
     resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Bad user.");
   }
-
-  public void setupRedirectPage(HttpServletResponse resp) {
-    try (PrintWriter html = resp.getWriter()) {
-      printWritrPageStart(html, "Writr: Submitted!");
-      // Print actual redirect directive:
-      html.println("<meta http-equiv=\"refresh\" content=\"3; url=front \">");
-
-      // Thank you, link.
-      html.println("<div class=\"body\">");
-      html.println("<div class=\"thanks\">");
-      html.println("<p>Thanks for your Submission!</p>");
-      html.println("<a href=\"front\">Back to the front page...</a> (automatically redirect in 3 seconds).");
-      html.println("</div>");
-      html.println("</div>");
-
-      printWritrPageEnd(html);
-
-    } catch (IOException ignored) {
-      // Don't consider a browser that stops listening to us after submitting a form to be an error.
-    }
-  }
-
 }
