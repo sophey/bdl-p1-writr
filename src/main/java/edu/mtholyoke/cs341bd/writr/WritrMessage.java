3f7bdafefaf4e8d9e6bd7267247dcc0d170f7e9d
John Foley <jfoley@cs.umass.edu>
2017-02-01 13:58:57 -0500

    maybe some more comments, jars

--- src/main/java/edu/mtholyoke/cs341bd/writr/WritrMessage.java
+++ src/main/java/edu/mtholyoke/cs341bd/writr/WritrMessage.java
@@ -6,7 +6,14 @@ import javax.annotation.Nonnull;
  * @author jfoley
  */
 public class WritrMessage implements Comparable<WritrMessage> {
+  /**
+   * This is the timestamp of when this message was added -- when the constructor gets called.
+   * We assume this is close enough to when the user presses the submit button for our uses.
+   *
+   * It's a long because it's the number of milliseconds since 1960... how computers tell time.
+   */
   long timeStamp;
+  /** The text the user typed in. */
   String messageText;
 
   /**
@@ -18,6 +25,10 @@ public class WritrMessage implements Comparable<WritrMessage> {
     timeStamp = System.currentTimeMillis();
   }
 
+  /**
+   * Rather than give a PrintWriter here, we'll use a StringBuilder, so we can quickly build up a string from all of the messages at once. I mostly did this a different way just to show it.
+   * @param output a stringbuilder object, to which we'll add our HTML representation.
+   */
   public void appendHTML(StringBuilder output) {
     output
         .append("<div class=\"message\">")
@@ -27,7 +38,8 @@ public class WritrMessage implements Comparable<WritrMessage> {
   }
 
   /**
-   * Sort newer messages to top by default.
+   * Sort newer messages to top by default. Maybe someday we'll sort in other ways.
+   *
    * @param o the other message to compare to.
    * @return comparator of (this, o).
    */
